08bc6b2595fe3421e256aba2c279ace91c3fd7e1
mohanselvam <mohanselvam2013@gmail.com>
2023-11-15 18:29:18 +0530

    The first java commited

---
+++ Test1.java
@@ -0,0 +1,11 @@
+package com.herokuapp.internetapp;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
